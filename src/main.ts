8c0ccef38061b6515835c449e617874b29a9b1ae
김진범 <thepolaris@jei.com>
2022-07-29 17:13:50 +0900

    update

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,8 @@ import Phaser from 'phaser';
 import MainScene from './scenes/MainScene';
 
 let platforms: Phaser.Physics.Arcade.StaticGroup;
+let player: Phaser.Types.Physics.Arcade.SpriteWithDynamicBody;
+let player2: Phaser.Types.Physics.Arcade.SpriteWithDynamicBody;
 
 const game = new Phaser.Game({
     type: Phaser.AUTO,
@@ -22,17 +24,46 @@ const game = new Phaser.Game({
             this.load.image('star', 'star.png');
             this.load.image('bomb', 'bomb.png');
             this.load.spritesheet('dude', 'dude.png', { frameWidth: 32, frameHeight: 48 });
+            this.load.spritesheet('dude1', 'dude.png', { frameWidth: 32, frameHeight: 48 });
         },
         create() {
             this.add.image(0, 0, 'sky').setOrigin(0, 0);
-
             platforms = this.physics.add.staticGroup();
-
             platforms.create(400, 568, 'ground').setScale(2).refreshBody();
-
             platforms.create(600, 400, 'ground');
             platforms.create(50, 250, 'ground');
             platforms.create(750, 220, 'ground');
+
+            player = this.physics.add.sprite(100, 450, 'dude');
+            player2 = this.physics.add.sprite(200, 450, 'dude1');
+
+            player.setCollideWorldBounds(true);
+            player2.setCollideWorldBounds(true);
+
+            this.physics.add.collider(player, platforms);
+
+
+            player.body.setGravityY(600)
+
+            this.anims.create({
+                key: 'left',
+                frames: this.anims.generateFrameNumbers('dude', { start: 0, end: 3 }),
+                frameRate: 10,
+                repeat: -1
+            });
+
+            this.anims.create({
+                key: 'turn',
+                frames: [{ key: 'dude', frame: 4 }],
+                frameRate: 20
+            });
+
+            this.anims.create({
+                key: 'right',
+                frames: this.anims.generateFrameNumbers('dude', { start: 5, end: 8 }),
+                frameRate: 10,
+                repeat: -1
+            });
         },
         update() {}
     }
